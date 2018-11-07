9997b1ac3a7167ee535dcd09496e5315b57020b3
kwmorris <k.w.morris@gmail.com>
2018-11-07 07:56:58 -0800

    Labwork

--- 2ndYear/CourseWork/INST241/Project2/f_channel_09.c
+++ 2ndYear/CourseWork/INST241/Project2/f_channel_09.c
@@ -12,14 +12,14 @@ Consult the "README.txt" file for help editing this function!
 int
 f_channel_09 (void)
 {
-  var vURV = 5; 	//input voltage Upper Range Value
-  var vLRV = 1;		//input voltage Lower Range Value
-  var tURV = 212;	//output temperature Upper Range Value
-  var tLRV = 32;	//output temperature Lower Range Value
+  int vURV = 5; 	//input voltage Upper Range Value
+  int vLRV = 1;		//input voltage Lower Range Value
+  int pURV = 30;	//output temperature Upper Range Value
+  int pLRV = 0;		//output temperature Lower Range Value
   
-  f_channel[9].value = (ain[9]-vLRV)/(vURV-vLRV)*(tURV-tLRV)+tLRV;
-  f_channel[9].tag = "TT-22";
-  f_channel[9].unit = "degF";
+  f_channel[9].value = (ain[9]-vLRV)/(vURV-vLRV)*(pURV-pLRV)+pLRV;
+  f_channel[9].tag = "FT-12";
+  f_channel[9].unit = "inWC";
   f_channel[9].status = 1;
   f_channel[9].comment = "Analog input AIN9";
 
---
+++ 2ndYear/CourseWork/INST241/Project2/f_channel_20.c.bak
@@ -0,0 +1,23 @@
+/*************************************************************************
+
+Consult the "README.txt" file for help editing this function!
+
+*************************************************************************/
+
+
+#include <stdio.h>
+#include <math.h>		// Necessary for any advanced math functions
+#include "cascada.h"		// Contains all the declarations specific to caSCADA
+
+int
+f_channel_20 (void)
+{
+
+  f_channel[20].value = 0;
+  f_channel[20].tag = "(none)";
+  f_channel[20].unit = "(none)";
+  f_channel[20].status = 1;
+  f_channel[20].comment = "(none)";
+
+  return 1;
+}
