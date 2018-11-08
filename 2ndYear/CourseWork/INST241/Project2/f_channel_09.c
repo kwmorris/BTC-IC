99b23d75de26dfacb8d68f7741dad5662de09d61
kwmorris <k.w.morris@gmail.com>
2018-11-07 18:02:11 -0800

    LabWork

--- 2ndYear/CourseWork/INST241/Project2/f_channel_09.c
+++ 2ndYear/CourseWork/INST241/Project2/f_channel_09.c
@@ -3,7 +3,7 @@
 Consult the "README.txt" file for help editing this function!
 
 *************************************************************************/
-
+/
 
 #include <stdio.h>
 #include <math.h>		// Necessary for any advanced math functions
@@ -14,14 +14,26 @@ f_channel_09 (void)
 {
   int vURV = 5; 	//input voltage Upper Range Value
   int vLRV = 1;		//input voltage Lower Range Value
-  int pURV = 30;	//output temperature Upper Range Value
-  int pLRV = 0;		//output temperature Lower Range Value
+  int pURV = 30;	//output pressure Upper Range Value
+  int pLRV = 0;		//output pressure Lower Range Value
+  float pLGV = 0.5;	//output pressure Lowest Good Value
+  float p;
+  int status;
   
-  f_channel[9].value = (ain[9]-vLRV)/(vURV-vLRV)*(pURV-pLRV)+pLRV;
+  p = (ain[9]-vLRV)/(vURV-vLRV)*(pURV-pLRV)+pLRV;
+  
+  if(p < pLGV){
+	  status = 0;
+	  f_channel[9].comment = "Low Flow Rate";
+  }else{
+	  status = 1;
+	  f_channel[9].comment = "Good Flow Rate";
+  }
+  
+  f_channel[9].value = p;
   f_channel[9].tag = "FT-12";
   f_channel[9].unit = "inWC";
-  f_channel[9].status = 1;
-  f_channel[9].comment = "Analog input AIN9";
+  f_channel[9].status = status;
 
   return 1;
 }
